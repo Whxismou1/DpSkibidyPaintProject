ebff3f7cd2f7e66daafc29d5347a20ab9df6c6a3
Whxismou1 <melouc00@estudiantes.unileon.es>
2024-10-23 10:51:40 +0200

    Modificacion bbddControlerr

--- src/main/java/com/skibidypaintproject/Controllers/BBDDController.java
+++ src/main/java/com/skibidypaintproject/Controllers/BBDDController.java
@@ -1,6 +1,9 @@
 package com.skibidypaintproject.Controllers;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public final class BBDDController {
 
@@ -9,15 +12,41 @@ public final class BBDDController {
     private String USER = dotenv.get("USER_DB");
     private String PASSWORD = dotenv.get("PASS_DB");
     private static BBDDController bbddInstance;
+    private Connection connectionBBDD;
 
     private BBDDController() {
+        try {
+            connectionBBDD = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static BBDDController getInstance() {
         if (bbddInstance == null) {
-            bbddInstance = new BBDDController();
+            synchronized (BBDDController.class) {
+                if (bbddInstance == null) {
+                    bbddInstance = new BBDDController();
+                }
+            }
         }
         return bbddInstance;
+    }
+
+    public Connection getConnection() {
+        return connectionBBDD;
+    }
+
+    public void closeConnection() {
+        if (connectionBBDD != null) {
+            try {
+                connectionBBDD.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+                throw new RuntimeException("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
     }
 
 }
