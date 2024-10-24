f6a52902eef3e336cd97985d5875452beb373a5d
Whxismou1 <melouc00@estudiantes.unileon.es>
2024-10-24 12:26:40 +0200

    Modificacion pom.xml(Conector mysql)

--- src/main/java/com/skibidypaintproject/Main.java
+++ src/main/java/com/skibidypaintproject/Main.java
@@ -1,5 +1,7 @@
 package com.skibidypaintproject;
 
+import com.skibidypaintproject.Controllers.BBDDController;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
