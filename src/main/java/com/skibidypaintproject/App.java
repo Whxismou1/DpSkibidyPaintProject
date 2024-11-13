d4f0e4efd09c67a28c51cdad828b764c559e29e9
Cristian <fenis357@gmail.com>
2024-11-13 10:40:07 +0100

    Arreglo diseño de Login/Signup

--- src/main/java/com/skibidypaintproject/App.java
+++ src/main/java/com/skibidypaintproject/App.java
@@ -22,7 +22,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
-        setRoot("planificador");
+        setRoot("register");
         primaryStage.setTitle("Skibidi Paint Project");
 
         primaryStage.setResizable(false);
